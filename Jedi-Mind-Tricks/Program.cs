c7871a10c03142570684e0a7ce2b4cc3c46b5b31
Driton Latifi <drit0046@zbc.dk>
2021-02-28 22:43:05 +0100

    Add project files.

---
+++ Jedi-Mind-Tricks/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jedi_Mind_Tricks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Opgave 1
+            Dictionary<string, int> droids = new Dictionary<string, int>();
+
+            // Opgave 2
+            droids.Add("Driton", 18);
+            droids["John"] = 19;
+            Console.WriteLine(droids.First());
+
+            // Opgave 3
+            Dictionary<string, bool> characters = new Dictionary<string, bool>()
+            {
+                { "Luke", true },
+                { "Han", false },
+                { "Chewbacca", false }
+            };
+            characters.Remove("han");
+
+            // Opgave 4
+            foreach (var item in droids)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
